832685b8949ec578063576d5341389c8ea4eddc6
Roberto Sintos <sintosroberto3@gmail.com>
2025-11-22 11:51:18 +0800

    Inital Commit

---
+++ Gender Perspectives The Philippine Story/src/App.jsx
@@ -0,0 +1,16 @@
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Hero from "./Pages/Hero";
+import "./assets/Style/App.css";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Hero />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
